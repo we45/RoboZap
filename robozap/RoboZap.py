9fece9d3291a26d4cb4a5d4914e9051c24f665cf
Abhay Bhargav <abhay@we45.com>
2018-01-27 06:46:03 -0800

    Changes to API ahead of AppSecCali. Added support for Export Report. Added GUI mode, etc.

--- robozap/RoboZap.py
+++ robozap/RoboZap.py
@@ -7,6 +7,8 @@ import base64
 import uuid
 import json
 from tinydb import TinyDB
+import requests
+from datetime import datetime
 
 import sys
 
@@ -16,21 +18,23 @@ sys.setdefaultencoding('UTF8')
 class RoboZap(object):
     ROBOT_LIBRARY_SCOPE = 'GLOBAL'
 
-    def __init__(self, proxy):
+    def __init__(self, proxy, port):
         '''
         ZAP Library can be imported with one argument
 
         Arguments:
-            - ``proxy``: Proxy is required to initialize the ZAP Proxy at that location
+            - ``proxy``: Proxy is required to initialize the ZAP Proxy at that location. This MUST include the port specification as well
+            - ``port``: This is a portspecification that will be used across the suite
 
 
         Examples:
 
         | = Keyword Definition =  | = Description =  |
 
-        | Library `|` RoboZap  | proxy|
+        | Library `|` RoboZap  | proxy | port |
         '''
         self.zap = ZAP(proxies={'http': proxy, 'https':proxy})
+        self.port = port
 
 
     def start_headless_zap(self, path):
@@ -39,13 +43,31 @@ class RoboZap(object):
 
         Examples:
 
-        | Start Headless ZAP  | path |
+        | start gui zap  | path | port |
 
         """
         try:
-            cmd = path + 'zap.sh -daemon -config api.disablekey=true -port 8090'
+            cmd = path + 'zap.sh -daemon -config api.disablekey=true -port {0}'.format(self.port)
             print cmd
             subprocess.Popen(cmd.split(' '), stdout = open(os.devnull, 'w'))
+            time.sleep(10)
+        except IOError as e:
+            print 'ZAP Path is not configured correctly'
+
+
+    def start_gui_zap(self, path):
+        """
+        Start OWASP ZAP with a GUI
+
+        Examples:
+
+        | start gui zap  | path | port |
+
+        """
+        try:
+            cmd = path + 'zap.sh -config api.disablekey=true -port {0}'.format(self.port)
+            print cmd
+            subprocess.Popen(cmd.split(' '), stdout=open(os.devnull, 'w'))
             time.sleep(10)
         except IOError as e:
             print 'ZAP Path is not configured correctly'
@@ -183,6 +205,66 @@ class RoboZap(object):
 
         return filename
 
+
+    def zap_write_to_orchy(self, token, hook_uri):
+        """
+                Generates an XML Report and writes said report to orchestron over a webhook.
+
+                Mandatory Fields:
+                - Token: Webhook Token
+                - hook_uri: the unique URI to post the XML Report to
+
+                Examples:
+
+                | zap write to orchy  | token | hook_uri
+
+        """
+        xml_report = self.zap.core.xmlreport()
+        with open('zap_scan.xml','w') as zaprep:
+            zaprep.write(xml_report)
+        try:
+            files = {'file': open('zap_scan.xml','rb')}
+            auth = {'Authorization': 'Token {0}'.format(token)}
+            r = requests.post(hook_uri, headers = auth, files = files)
+            if r.status_code == 200:
+                return "Successfully posted to Orchestron"
+            else:
+                return "Failed writing to Orchy Console"
+        except Exception as e:
+            print(e)
+
+
+    def zap_export_report(self, export_file, export_format, report_title, report_author):
+        """
+        This functionality works on ZAP 2.7.0 only. It leverages the Export Report Library to generate a report.
+        Currently ExportReport doesnt have an API endpoint in python. We will be using the default ZAP REST API for this
+        :param export_file: location to which the export needs to happen. Absolute path with the export file name and extension
+        :param export_format: file extension of the exported file. Can be XML, JSON, HTML, PDF, DOC
+        :param report_title: Title of the exported report
+        :param report_author: Name of the Author of the report
+        Examples:
+
+        | zap export report | export_path | export_format |
+
+        """
+
+        url = 'http://localhost:{0}/JSON/exportreport/action/generate/'.format(self.port)
+        export_path = export_file
+        extension = export_format
+        report_time = datetime.now().strftime("%I:%M%p on %B %d, %Y")
+        source_info = '{0};{1};ZAP Team;{2};{3};v1;v1;{4}'.format(report_title, report_author, report_time, report_time, report_title)
+        alert_severity = 't;t;t;t'  # High;Medium;Low;Info
+        alert_details = 't;t;t;t;t;t;f;f;f;f'  # CWEID;#WASCID;Description;Other Info;Solution;Reference;Request Header;Response Header;Request Body;Response Body
+        data = {'absolutePath': export_path, 'fileExtension': extension, 'sourceDetails': source_info,
+                'alertSeverity': alert_severity, 'alertDetails': alert_details}
+
+        r = requests.post(url, data=data)
+        if r.status_code == 200:
+            pass
+        else:
+            raise Exception("Unable to generate report")
+
+
     def zap_write_to_tiny(self, base_url, db_name, app_name):
         """
         Fetches all the results from zap.core.alerts() and writes to json file.
@@ -192,6 +274,9 @@ class RoboZap(object):
         | zap write to json  | scan_id |
 
         """
+        # xml_report = self.zap.core.xmlreport()
+        # with open('zap_scan.xml','w') as zaprep:
+        #     zaprep.write(xml_report)
         db = TinyDB(os.environ.get('VUL_DB', db_name))
         core = self.zap.core
         scan = str(uuid.uuid4())
